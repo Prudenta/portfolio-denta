3b2391783762ac818560cda616d5eba80a5c10b2
Prudenta <zxkintilzxkintilexp@gmail.com>
2025-01-06 20:33:56 +0700

    Upload rev 0.2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-// script.js
-
-// Function to display greeting based on time
 function displayGreeting() {
     const greetingElement = document.getElementById('greeting');
     const currentHour = new Date().getHours();
@@ -16,11 +13,8 @@ function displayGreeting() {
 
     greetingElement.textContent = greetingMessage;
 }
-
-// Call the function when the page loads
 window.onload = displayGreeting;
 
-// Gallery Filter
 document.querySelectorAll('.btn-outline-primary').forEach(button => {
     button.addEventListener
     ('click', function() {
@@ -35,7 +29,6 @@ document.querySelectorAll('.btn-outline-primary').forEach(button => {
     });
 });
 
-// Contact Form Validation
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
